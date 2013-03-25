bf99921fe4c30e2d678ef840d9f32d46b05bd9e0
david <david.trattnig@gmail.com>
2013-03-25 20:19:29 +0100

    initial GeoResource domain

---
+++ grails-app/domain/sq/maps/GeoResource.groovy
@@ -0,0 +1,11 @@
+package sq.maps
+
+class GeoResource {
+
+	Long lat
+	Long lng
+	Integer zoom
+	
+    static constraints = {
+    }
+}
---
+++ test/unit/sq/maps/GeoResourceTests.groovy
@@ -0,0 +1,17 @@
+package sq.maps
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(GeoResource)
+class GeoResourceTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
